56a6e691dd35e1f4443e9c01ca36f73b31f80709
Xenonica <dragnobi@gmail.com>
2021-02-16 00:03:17 +0800

    QRCode package installation and initial MC template

---
+++ heDeno/PatientMC.aspx.cs
@@ -0,0 +1,48 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace heDeno
+{
+    public partial class PatientMC : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["user"] != null && Session["AuthToken"] != null && Request.Cookies["AuthToken"] != null)
+            {
+                if (!Session["AuthToken"].ToString().Equals(Request.Cookies["AuthToken"].Value))
+                {
+                    Response.Redirect("/Login", false);
+                }
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode($"http://localhost:50198/PatientMC \nDate Scanned: {DateTime.Now}", QRCodeGenerator.ECCLevel.Q);
+                QRCode qrCode = new QRCode(qrCodeData);
+
+                System.Web.UI.WebControls.Image imgBarCode = new System.Web.UI.WebControls.Image();
+                imgBarCode.Height = 150;
+                imgBarCode.Width = 150;
+
+                using (Bitmap bitMap = qrCode.GetGraphic(20))
+                {
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        bitMap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                        byte[] byteImage = ms.ToArray();
+                        imgBarCode.ImageUrl = "data:image/png;base64," + Convert.ToBase64String(byteImage);
+                    }
+                    placeholder_qrcode.Controls.Add(imgBarCode);
+                }
+            }
+            else
+            {
+                Response.Redirect("/Login", false);
+            }
+        }
+    }
+}
